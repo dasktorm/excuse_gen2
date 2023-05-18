16f43519831c48f03e36869d7e97aff3fa7259a0
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2023-05-18 18:08:48 +0000

    fixed-files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
-const who = ['The dog','My grandma','His turtle','My bird'];
-const action = ['ate','peed','crushed','broke'];
-const what = ['my homework', 'the keys', 'the car'];
-const when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+const who = ["The dog", "My grandma", "His turtle", "My bird"];
+const action = ["ate", "peed", "crushed", "broke"];
+const what = ["my homework", "the keys", "the car"];
+const when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 function generateExcuse() {
   const randomWho = who[Math.floor(Math.random() * who.length)];
   const randomAction = action[Math.floor(Math.random() * action.length)];
@@ -10,7 +16,7 @@ function generateExcuse() {
   const excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
   return excuse;
 }
-document.getElementById('generate-btn').addEventListener('click', function() {
+document.getElementById("generate-btn").addEventListener("click", function() {
   const excuse = generateExcuse();
-  document.getElementById('excuse').innerHTML = excuse;
+  document.getElementById("excuse").innerHTML = excuse;
 });
